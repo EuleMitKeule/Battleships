b31d7241a65e66ad8ca2b1739960a591c255ddb4
michimalek <mchlmalek00@gmail.com>
2021-01-10 19:41:21 +0100

    Update AiHasGuessedCorrectlyState.java

--- src/main/java/game/AiHasGuessedCorrectlyState.java
+++ src/main/java/game/AiHasGuessedCorrectlyState.java
@@ -16,6 +16,7 @@ public class AiHasGuessedCorrectlyState implements IAiState
     {
         if (isSunk)
         {
+            System.out.println("Schiff versenkt. Fängt von vorne an");
             _computer.setState(_computer.aiStartState);
             _computer.state.onUpdate(player, cellPos, false, false);
             return;
@@ -23,6 +24,7 @@ public class AiHasGuessedCorrectlyState implements IAiState
 
         if (!isHit)
         {
+            System.out.println("Nicht getroffen");
             _computer.setState(_computer.aiHasGuessedIncorrectlyState);
             _computer.state.onUpdate(player, cellPos, isHit, isSunk);
             return;
@@ -32,33 +34,41 @@ public class AiHasGuessedCorrectlyState implements IAiState
         var leftPos = _computer.lastGuessPos.add(Vector2Int.left());
         var upPos = _computer.lastGuessPos.add(Vector2Int.up());
         var downPos = _computer.lastGuessPos.add(Vector2Int.down());
+        var nextPos = _computer.lastGuessPos.add(Direction.ToVector(_computer.curDirection));
 
-        if (_computer.ownBoard.canGuess(rightPos))
+        if (_computer.ownBoard.canGuess(nextPos))
         {
-            _computer.lastGuessPos = rightPos;
-            _computer.invokeMove(rightPos);
-        }
-        else if (_computer.ownBoard.canGuess(leftPos))
-        {
-            _computer.lastGuessPos = leftPos;
-            _computer.invokeMove(leftPos);
-        }
-        else if (_computer.ownBoard.canGuess(upPos))
-        {
-            _computer.lastGuessPos = upPos;
-            _computer.invokeMove(upPos);
-        }
-        else if (_computer.ownBoard.canGuess(downPos))
-        {
-            _computer.lastGuessPos = downPos;
-            _computer.invokeMove(downPos);
+            System.out.println("Geht nach " + _computer.curDirection);
+            _computer.lastGuessPos = nextPos;
+            _computer.invokeMove(nextPos);
         }
         else
         {
+            System.out.println("Ging nicht");
             _computer.setState(_computer.aiStartState);
             _computer.state.onUpdate(player, cellPos, false, false);
             return;
         }
+        // if (_computer.ownBoard.canGuess(rightPos))
+        // {
+        //     _computer.lastGuessPos = rightPos;
+        //     _computer.invokeMove(rightPos);
+        // }
+        // else if (_computer.ownBoard.canGuess(leftPos))
+        // {
+        //     _computer.lastGuessPos = leftPos;
+        //     _computer.invokeMove(leftPos);
+        // }
+        // else if (_computer.ownBoard.canGuess(upPos))
+        // {
+        //     _computer.lastGuessPos = upPos;
+        //     _computer.invokeMove(upPos);
+        // }
+        // else if (_computer.ownBoard.canGuess(downPos))
+        // {
+        //     _computer.lastGuessPos = downPos;
+        //     _computer.invokeMove(downPos);
+        // }
     }
 
     @Override
